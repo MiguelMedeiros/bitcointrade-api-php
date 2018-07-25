fec5198ec8fc16e2592dab5ac9ce49169d6ab5e0
jaonoctus <joaodias@noctus.org>
2018-07-25 15:14:53 -0300

    PSR https://www.php-fig.org/psr/
    > PSR-1
    3. Namespace and Class Names
    - Class names MUST be declared in StudlyCaps.
    
    > PSR-2
    2.2. Files
    - All PHP files MUST end with a single blank line.
    - The closing tag MUST be omitted from files containing only PHP.
    2.4. Indenting
    - Code MUST use an indent of 4 spaces, and MUST NOT use tabs for indenting.
    4.3. Methods
    - Method names MUST NOT be declared with a space after the method name. The
    opening brace MUST go on its own line, and the closing brace MUST go on the
    next line following the body. There MUST NOT be a space after the opening
    parenthesis, and there MUST NOT be a space before the closing parenthesis.
    4.4. Method Arguments
    - In the argument list, there MUST NOT be a space before each comma, and there
    MUST be one space after each comma.
    - Argument lists MAY be split across multiple lines, where each subsequent line
    is indented once. When doing so, the first item in the list MUST be on the
    next line, and there MUST be only one argument per line.
    - When the argument list is split across multiple lines, the closing parenthesis
    and opening brace MUST be placed together on their own line with one space
    between them.
    4.6. Method and Function Calls
    - When making a method or function call, there MUST NOT be a space between the
    method or function name and the opening parenthesis, there MUST NOT be a space
    after the opening parenthesis, and there MUST NOT be a space before the
    closing parenthesis. In the argument list, there MUST NOT be a space before
    each comma, and there MUST be one space after each comma.

--- index.php
+++ index.php
@@ -1,82 +1,80 @@
 <?php
-    // **********************************************
-    // Exemplo de uso Exchange www.BitcoinTrade.com.br
-    // **********************************************
-  
-    // importar classe
-    require ("bitcointrade.php");
+// **********************************************
+// Exemplo de uso Exchange www.BitcoinTrade.com.br
+// **********************************************
 
-    // Cria instancia da classe Bitcoin Trade
-    $apiKey = '';
-    $exchange = new bitcoinTrade($apiKey);
-    
-    // variáveis para parametrização
-    $currency = 'BTC';
-    $hours = 1;
-    $page_size = 100;
-    $current_page = 1;   
+// importar classe
+require("bitcointrade.php");
 
-    // get ticker
-    //$ticker = $exchange->ticker($currency);
-    //var_dump($ticker->data);
+// Cria instancia da classe Bitcoin Trade
+$apiKey = '';
+$exchange = new BitcoinTrade($apiKey);
 
-    // get orders
-    //$orders = $exchange->orders($currency);
-    //var_dump($orders->data);
+// variáveis para parametrização
+$currency = 'BTC';
+$hours = 1;
+$page_size = 100;
+$current_page = 1;
 
-    // get trades
-    //$trades = $exchange->trades($currency, $hours, $page_size, $current_page);
-    //var_dump($trades->data);
+// get ticker
+$ticker = $exchange->ticker($currency);
+var_dump($ticker->data);
 
-    // get orderbook
-    //$orderbook = $exchange->orderbook($currency);
-    //var_dump($orderbook->data);
-    //var_dump($orderbook->data->buying);
-    //var_dump($orderbook->data->selling);
-    //var_dump($orderbook->data->executed);
+// get orders
+$orders = $exchange->orders($currency);
+var_dump($orders->data);
 
-    // get summary
-    //$summary = $exchange->summary($currency);
-    //var_dump($summary->data);
+// get trades
+$trades = $exchange->trades($currency, $hours, $page_size, $current_page);
+var_dump($trades->data);
 
-    
-    // create order
-    // buy
-    $amount = 0.001; // Quantidade da moeda - (FLOAT)
-    $type ="buy"; // Tipo da ordem - buy / sell (STRING)
-    $subtype = "limited"; // Subtipo da ordem - limited / market (STRING)
-    $unitPrice = 10000; // Preço de unidade da moeda - (FLOAT)
-    //$buyOrder = $exchange->createOrder($currency, $amount, $type, $subtype, $unitPrice);
-    //var_dump($buyOrder);
+// get orderbook
+$orderbook = $exchange->orderbook($currency);
+var_dump($orderbook->data);
+var_dump($orderbook->data->buying);
+var_dump($orderbook->data->selling);
+var_dump($orderbook->data->executed);
 
-    // sell
-    $amount = 0.001; // Quantidade da moeda - (FLOAT)
-    $type ="sell"; // Tipo da ordem - buy / sell (STRING)
-    $subtype="limited"; // Subtipo da ordem - limited / market (STRING)
-    $unitPrice = 30000; // Preço de unidade da moeda - (FLOAT)
-    //$sellOrder = $exchange->createOrder($currency, $amount, $type, $subtype, $unitPrice);
-    //var_dump($sellOrder);
+// get summary
+$summary = $exchange->summary($currency);
+var_dump($summary->data);
 
-    // cancel order
-    $orderId = 'U2FsdGVkX1+qrxVqoDbSYh9wCvhhrP63SMy01Sn27Ag=';
-    //$cancelOrder = $exchange->cancelOrder($orderId);
-    //var_dump($cancelOrder);
 
-    // get user orders
-    $status = 'waiting'; // Status da ordem - executed_completely / executed_partially / waiting / canceled (STRING)
-    $hours = 24; // últimas 24 hras
-    $type = "sell"; // Tipo da ordem - buy / sell (STRING)
-    //$userOrders = $exchange->userOrders($currency, $status, $hours, $type, $page_size, $current_page);
-    //var_dump($userOrders->data);
+// create order
+// buy
+$amount = 0.001; // Quantidade da moeda - (FLOAT)
+$type ="buy"; // Tipo da ordem - buy / sell (STRING)
+$subtype = "limited"; // Subtipo da ordem - limited / market (STRING)
+$unitPrice = 10000; // Preço de unidade da moeda - (FLOAT)
+$buyOrder = $exchange->createOrder($currency, $amount, $type, $subtype, $unitPrice);
+var_dump($buyOrder);
 
-    // estimated price
-    $amount = 100;
-    $type ="buy";
-    //$estimatedPrice = $exchange->estimatedPrice($currency, $amount, $type);
-    //var_dump($estimatedPrice->data);
+// sell
+$amount = 0.001; // Quantidade da moeda - (FLOAT)
+$type ="sell"; // Tipo da ordem - buy / sell (STRING)
+$subtype="limited"; // Subtipo da ordem - limited / market (STRING)
+$unitPrice = 30000; // Preço de unidade da moeda - (FLOAT)
+$sellOrder = $exchange->createOrder($currency, $amount, $type, $subtype, $unitPrice);
+var_dump($sellOrder);
 
-    // balance
-    //$balance = $exchange->balance();
-    //var_dump($balance->data);
+// cancel order
+$orderId = 'U2FsdGVkX1+qrxVqoDbSYh9wCvhhrP63SMy01Sn27Ag=';
+$cancelOrder = $exchange->cancelOrder($orderId);
+var_dump($cancelOrder);
 
-?>
+// get user orders
+$status = 'waiting'; // Status da ordem - executed_completely / executed_partially / waiting / canceled (STRING)
+$hours = 24; // últimas 24 hras
+$type = "sell"; // Tipo da ordem - buy / sell (STRING)
+$userOrders = $exchange->userOrders($currency, $status, $hours, $type, $page_size, $current_page);
+var_dump($userOrders->data);
+
+// estimated price
+$amount = 100;
+$type ="buy";
+$estimatedPrice = $exchange->estimatedPrice($currency, $amount, $type);
+var_dump($estimatedPrice->data);
+
+// balance
+$balance = $exchange->balance();
+var_dump($balance->data);
